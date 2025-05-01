691f75cb80307afc6798fc33440244b6bc73a56b
jaywebcoder <jay8053333@gmail.com>
2025-05-01 16:24:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth Scroll
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      if (this.getAttribute('href').startsWith("#")) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+  
